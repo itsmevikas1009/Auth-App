5bdd55ec1001a6a27e376a175a1c0a3789b4aa29
Vikas Chaurasia <chaurasiavikas100904@gmail.com>
2024-03-02 01:03:25 +0530

    Auth-App

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -13,39 +13,73 @@ function Dashboard({ user, setUser }) {
       navigate("/");
     }
     document.title = "Auth App - Home";
-  }, [user]);
+  }, []);
 
-  return (
-    <>
-      <NavBar user={user} setUser={setUser} />
-      <div className="pt-16">
-        {user ? (
-          user.accountType === "Admin" ? (
-            <AdminDashboard />
-          ) : user.accountType === "User" ? (
-            <div className="lg:flex lg:justify-center lg:items-center text-center lg:pt-16">
-              <div>
-                <div className="">
-                  <h1 className="sm:text-4xl sm:m-5 sm:mt-0 mt-4 text-2xl dark:text-white text-gray-900">
-                    Hello{" "}
-                    <span className="text-[#bfe1d4] dark:text-[#00df9a]">
-                      {user.firstName},
-                    </span>{" "}
-                    Welcome to
-                  </h1>
-                </div>
-                <Animation />
+  if (user.accountType === "Admin") {
+    return (
+      <>
+        <NavBar user={user} setUser={setUser} />
+        <div className="pt-16">
+          <AdminDashboard />
+        </div>
+      </>
+    );
+  } else if (user.accountType === "User") {
+    return (
+      <>
+        <NavBar user={user} setUser={setUser} />
+        <div className="pt-16">
+          <div className="lg:flex lg:justify-center lg:items-center text-center lg:pt-16">
+            <div>
+              <div className="">
+                <h1 className="sm:text-4xl sm:m-5 sm:mt-0 mt-4 text-2xl dark:text-white text-gray-900">
+                  Hello{" "}
+                  <span className="text-[#bfe1d4] dark:text-[#00df9a]">
+                    {user.firstName},
+                  </span>{" "}
+                  Welcome to
+                </h1>
               </div>
+              <Animation />
             </div>
-          ) : (
-            setUser()
-          )
-        ) : (
-          setUser()
-        )}
-      </div>
-    </>
-  );
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
+  // return (
+  //   <>
+  //     <NavBar user={user} setUser={setUser} />
+  //     <div className="pt-16">
+  //       {user ? (
+  //         user.accountType === "Admin" ? (
+  //           <AdminDashboard />
+  //         ) : user.accountType === "User" ? (
+  //           <div className="lg:flex lg:justify-center lg:items-center text-center lg:pt-16">
+  //             <div>
+  //               <div className="">
+  //                 <h1 className="sm:text-4xl sm:m-5 sm:mt-0 mt-4 text-2xl dark:text-white text-gray-900">
+  //                   Hello{" "}
+  //                   <span className="text-[#bfe1d4] dark:text-[#00df9a]">
+  //                     {user.firstName},
+  //                   </span>{" "}
+  //                   Welcome to
+  //                 </h1>
+  //               </div>
+  //               <Animation />
+  //             </div>
+  //           </div>
+  //         ) : (
+  //           window.location.reload()
+  //         )
+  //       ) : (
+  //         window.location.reload()
+  //       )}
+  //     </div>
+  //   </>
+  // );
 }
 
 export default Dashboard;
